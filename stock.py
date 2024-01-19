46a2b8b54f4eecc5cfca55c4d0f3fd3a275e1a75
Jeffma0103 <tojeffma@gmail.com>
2024-01-19 15:31:36 +0800

    stock

--- stock.py
+++ stock.py
@@ -1,2 +1,19 @@
-import time
 import twstock
+import requests
+class GetData:
+    def __init__(self, stockid):
+        self.stockid = stockid
+
+    def get_price(self):
+        price = twstock.realtime.get(self.stockid)
+        if price['success']:
+            return (price['info']['name'], float(price['realtime']['latest_trade_price']))
+        else:
+            return (False, False)
+
+
+
+
+test = GetData("2330")
+
+test.get_price()
